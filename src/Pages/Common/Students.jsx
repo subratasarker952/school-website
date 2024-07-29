723c314d69e37a032a43ef76a81775481adea4b9
subrata sarker <tccdcc8@gmail.com>
2024-07-29 13:32:05 +0600

    m

--- src/Pages/Common/Students.jsx
+++ src/Pages/Common/Students.jsx
@@ -7,13 +7,13 @@ const Students = () => {
           <span className="font-bold">Life at Springdale:- </span>
           <div className="flex flex-col gap-3">
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
-              <span className="font-bold">Extracurricular Activities:</span>{" "}
+              <span className="text-sm font-bold">o</span>
+              <span className="font-bold">Extracurricular Activities:</span>
               Music, Dance, Drama, Art, Sports, Robotics, Debate Club, Science
               Club.
             </p>
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">Clubs and Societies:</span> Literary
               Society, Environmental Club, Astronomy Club, Coding Club.
             </p>
@@ -23,17 +23,17 @@ const Students = () => {
           <span className="font-bold">Achievements:- </span>
           <div className="flex flex-col gap-3">
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">John Smith -</span> National Level
               Math Olympiad Winner.
             </p>
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">Sarah Lee -</span> Gold Medalist in
               State Swimming Championship
             </p>
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">Tech Innovators Club -</span> Winners
               of Inter-School Robotics Competition
             </p>
@@ -43,17 +43,17 @@ const Students = () => {
           <span className="font-bold">Student Council:- </span>
           <div className="flex flex-col gap-3">
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">President:</span> Amy Parker, Grade
               12.
             </p>
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold">Vice President: </span> Rajiv Mehta,
               Grade 11
             </p>
             <p className="ml-10">
-              <span className="text-sm font-bold">o</span>{" "}
+              <span className="text-sm font-bold">o</span>
               <span className="font-bold"> Secretary: </span> Lisa Wong, Grade
               10..
             </p>
